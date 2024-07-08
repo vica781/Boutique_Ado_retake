0ad8d45cd0c8785b256b85c6c16d74509bedb8f5
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-07-08 13:33:24 +0100

    Feat: add home app and templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth', # required for allauth
     'allauth.account', # required for allauth
     'allauth.socialaccount', # required for allauth
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -60,12 +61,15 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required for allauth
+                'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
